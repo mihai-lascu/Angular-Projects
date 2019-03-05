7b108d50d7e19a14835f0a3c5ff6f3fcb8ce6741
Mihsi <lascu.mihai.teodor@gmail.com>
2019-03-05 19:57:11 +0200

    Added game rules and dice combos drop downs via rules component.

--- Barbarosa/src/app/app.module.ts
+++ Barbarosa/src/app/app.module.ts
@@ -3,10 +3,12 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {RulesComponent} from './rules/rules.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RulesComponent,
   ],
   imports: [
     BrowserModule,
