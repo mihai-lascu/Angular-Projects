57036493f53000184298f3caad13fd117a464fd7
mihai-lascu <lascu.mihai.teodor@gmail.com>
2021-11-01 18:56:01 +0200

    material module and html stuff

--- genghis/src/app/app.module.ts
+++ genghis/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+import { AppMaterialModule } from './app-material.module'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AppMaterialModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
