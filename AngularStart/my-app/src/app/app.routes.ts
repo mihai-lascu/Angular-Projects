e5f6ceff93ec069055d925aadd4676f83a29c20c
mihai-lascu <lascu.mihai.teodor@gmail.com>
2023-11-25 12:06:15 +0200

    Finish Lesson 2

--- AngularStart/my-app/src/app/app.routes.ts
+++ AngularStart/my-app/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'home',
+		loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent)
+	},
+	{
+		path: 'settings',
+		loadComponent: () => import('./settings/settings.component').then((m) => m.SettingsComponent)
+	},
+	{
+		path: '',
+		redirectTo: 'home',
+		pathMatch: 'full'
+	}
+];
--- AngularStart/my-app/src/app/home/home.component.ts
+++ AngularStart/my-app/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {WelcomeComponent} from "./ui/welcome/welcome.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+	imports: [CommonModule, WelcomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
